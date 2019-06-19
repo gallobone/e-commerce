1079ae6d2bc8ac67d9227dffe40e7fba7d4df9e0
gallobone <orsi.f84@gmail.com>
2019-06-19 02:51:53 -0300

    created prj obraloh

---
+++ configApp.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$appId = '5427097521058868';
+
+$secretKey = 'bWDoWa4H7rj0ZsXnXo6Wai401N0cTqwf';
+
+$redirectURI = 'http://localhost/obraloh/index.php';
+
+$siteId = 'MLB';
+
+
---
+++ header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=100, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>APP OBRALOH</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css"
+          integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+
+     <style type="text/css">
+     .bg-dark{
+     	background-color: #00182f!important;
+     }
+
+     </style>
+</head>
+<body>
+<?php
+include_once 'checklogin.php';
+include_once 'menu.php';
+?>
